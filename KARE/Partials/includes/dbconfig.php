dd1c8799c679817b1b8baf217cf62b9929cb0770
Al Evan Castillo <castillo.alevan143@gmail.com>
2022-03-15 20:35:03 +0800

    firebase php

---
+++ KARE/Partials/includes/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+
+// $serviceAccount = ServiceAccount::fromJsonFile(__DIR__. 'react-ebe8e-firebase-adminsdk-edc3p-3832332702.json');
+$firebase = (new Factory)
+    ->withServiceAccount(__DIR__ . '/database.json')
+    ->withDatabaseUri($config['firebase-uri']);
+
+$database = $firebase->createDatabase();
